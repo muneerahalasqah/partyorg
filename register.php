b7ad2dc1677cf80033897f983f54eda3dac11f38
muneerahalasqah <30496874+muneerahalasqah@users.noreply.github.com>
2018-02-12 23:16:31 +0300

    modified customer registring

--- register.php
+++ register.php
@@ -1,47 +1,44 @@
-<?php 
-//session_start();
+<?php
+$db = mysqli_connect('localhost','root','','partyorg');
 
+//هذا يصير بحالة ضغط زر الريجستر
+if(isset($_POST['submit'])){
+    $fname=$_POST['fname'];
+    $lname=$_POST['lname'];
+    $password=$_POST['password'];
+    $pass_confirm=$_POST['pass_confirm'];
+    $email=$_POST['email'];
+    $location=$_POST['location'];
+    
+    //echo $fname.$lname.$password.$pass_confirm.$email.$email.$location;
+    
+    //يتأكد من الباسوور لو يتطابفون أو لا
+    if($password==$pass_confirm){
+     $sql = "INSERT INTO customer(fname,lname,email,password,location_id) VALUES('$fname','$lname','$email','$password','$location')";
+    $query = mysqli_query($db,$sql);
+        if($query){
+             ?>
+        <html>
+            <body>
+            <script> alert('You have been registered sucessfully!'); 
+            window.location = "index.php";
+            </script>
+            </body>
+        </html>
+        <?php 
+        } else {
+             ?>
+        <html>
+            <body>
+            <script> alert('there was a proplem'); 
+            //window.location = "vendor.php";
+            </script>
+            </body>
+        </html>
+        <?php 
+        }
+    }
+    
 
-try 
-{
-$db = new PDO('mysql:host=localhost;dbname=partyorg', 'root', '');
-} 
-catch(PDOException $e)
-{
-  die('error'. $e->getMessage());
-}
-
-
-
-if (isset($_POST['submit']))
-{
-  $fname  = $_POST['fname'];
-  $lname = $_POST['lname'];
-  $email = $_POST['email'];
-  $password = $_POST['password'];
-  $pass_confirm = $_POST['pass_confirm'];
-  $location  = $_POST['location'];
-
-// معالجة البيانات 
-// ...
-// إدخال البيانات في القاعدة
-  $stmt= $db->prepare('INSERT INTO customer (fname, lname, email, password , location_id) 
-              VALUES (:name1,:name2, :mail, :pass1 , :city)
-            ');
-    $stmt->bindValue(':name1',$fname,PDO::PARAM_STR);
-    $stmt->bindValue(':name2',$lname,PDO::PARAM_STR);
-    $stmt->bindValue(':mail',$email,PDO::PARAM_STR);
-    $stmt->bindValue(':pass1',$password,PDO::PARAM_STR);
-    $stmt->bindValue(':city',$location,PDO::PARAM_STR);
-
-    $stmt->execute();
-
-    $stmt->CloseCursor();
-
-    // تعيين جلسة لإشعار المستخدم بنجاح عملية تسجيله ، ثم تحويله لصفحة الدخول
-   // $_SESSION['message'] = htmlspecialchars($fname). 'successful';
-  
-    header('location:index.html'); exit;
 }
 ?>
-
