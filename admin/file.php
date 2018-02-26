5386a4cccad10a4d1586ede1e3b93fc53e91eded
unknown <nor-os@hotmail.com>
2018-02-25 22:25:37 -0800

    file upload

--- admin/file.php
+++ admin/file.php
@@ -1,6 +1,6 @@
  <?php   
 
-
+$db=mysqli_connect("localhost","root","","partyorg");
 $name = $_FILES['samples']['name'];
 $tmp_name = $_FILES ['samples']['tmp_name'];
 
@@ -9,7 +9,17 @@ if (isset($name)){
         $location = '';
         
         if (move_uploaded_file($tmp_name,$location.$name)){
-            echo 'uploaded!';
+            echo 'uploaded! ';
+            $path="admin/".$name;
+            echo $path;
+            $vid=11;
+            $sql = "INSERT INTO samples(sample_path,v_id) VALUES($path,$vid)";
+            $query = mysqli_query($db,$sql);
+            if($query){
+                echo "Added!!!!";
+            } else {
+                echo "Noo";
+            }
         }}
         else {
             echo 'choose file';
