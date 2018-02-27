90d8a29680ee10bd354b4cc8746e01f48d33b2ce
unknown <nor-os@hotmail.com>
2018-02-27 11:48:35 -0800

    footer

--- account.php
+++ account.php
@@ -1,32 +1,33 @@
 <?php
 session_start();
 ?>
-<!ODCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="description" content="">
-        <meta name="author" content="">
-        <!-- Title & Logo -->
-        <title>Besher - بِشر</title>
-        <link rel="shortcut icon"
-           href="img/logo21.png"width="16" height="16"
-           />
-            <!-- Bootstrap core CSS -->
-        <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Title & Logo -->
+    <title>Besher - بِشر</title>
+    <link rel="shortcut icon"
+       href="img/logo21.png"width="16" height="16"/>
+      
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-        <!-- Custom fonts for this template -->
-        <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-        <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
-        <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-        <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
 
-        <!-- Custom styles for this template -->
-        <link href="css/agency.min.css" rel="stylesheet">
-        <link href="css/login-register.css" rel="stylesheet">
-        
+    <!-- Custom styles for this template -->
+    <link href="css/agency.min.css" rel="stylesheet">
+    <link href="css/login-register.css" rel="stylesheet">
+      
+      
                 <!-- Bootstrap core JavaScript -->
         <script src="vendor/jquery/jquery.min.js"></script>
         <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -40,83 +41,86 @@ session_start();
 
         <!-- Custom scripts for this template -->
         <script src="js/agency.min.js"></script>
-    </head>
-    
-    <body id="page-top">
-        <!-- Navigation -->
-        <nav style="background-color:#5b6771;" class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
-            <div class="container">
-                
-                <!-- Login& Profile Buttons -->
-                <?php
-                if(isset($_SESSION['cid'])){
-                    $cid = $_SESSION['cid'];
-                    $db=mysqli_connect('localhost','root','','partyorg');
-                    $result=mysqli_query($db,"SELECT fname FROM customer WHERE customer_id=$cid");
-                    $row=mysqli_fetch_row($result);
-                    $fname=$row[0];  
-                ?>
-                <div class="dropdown">
-                <button class="dropbtn" type="button"><i class="fa fa-chevron-circle-down"></i> <?php echo $fname;?></button>
-                <div class="dropdown-content">
-                    <a href="account.php"><i class="fa fa-user"></i> Account</a>
-                    <a href="#"><i class="fa fa-bars"></i> Party Plan</a>
-                    <a href="logout.php"><i class="fa fa-power-off"></i> Logout</a>
-                </div>
-                </div>
-                <?php
-                } else { 
-                ?>
-                <button class="login-btn" type="button" onclick="document.getElementById('id01').style.display='block'">Login</button>
-                <?php } ?>
-                
-                <!-- Logo to the left -->
-                <a class="navbar-brand js-scroll-trigger" href="index.php"><img src ="img/Logo33.png"width="80" height="40"/></a>
-                <!-- Navigation Mennu -->
-                <button class="navbar-toggler navbar-toggler-left" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                Menu
-                <i class="fa fa-bars"></i>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav text-uppercase ml-auto text-left">
-                    <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="#about">About us</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="#portfolio">Categories</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="faq.html">FAQ</a>
-                    </li>
-                </ul>
-                </div>
+  </head>
+
+  <body id="page-top" style="background-color:#f1f1f1">
+
+    <!-- Navigation -->
+    <nav style="background-color:#5b6771;" class="navbar navbar-expand-lg navbar-dark fixed-top " id="mainNav" >
+      
+        <div class="container">
+          <!-- Login Modal & Button -->
+        <?php
+        if (isset($_SESSION['cid'])){
+            $cid = $_SESSION['cid'];
+            $db=mysqli_connect('localhost','root','','partyorg');
+            $result=mysqli_query($db,"SELECT fname FROM customer WHERE customer_id=$cid");
+            $row=mysqli_fetch_row($result);
+            $fname=$row[0];
+            ?>
+            <div class="dropdown">
+            <button class="dropbtn" type="button"><i class="fa fa-chevron-circle-down"></i> <?php echo $fname;?></button>
+            <div class="dropdown-content">
+                <a href="account.php"><i class="fa fa-user"></i> Account</a>
+                <a href="#"><i class="fa fa-bars"></i> Party Plan</a>
+                <a href="logout.php"><i class="fa fa-power-off"></i> Logout</a>
             </div>
-                
-                <!-- Login Modal -->
-                <div id="id01" class="modal">
-                    <!-- Login Form -->
-                    <form class="loginform modal-content animate" action="login.php" method="POST">
-                       <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
-                        <div class="imgcontainer">
-                            <img src="img/Logo33.png" alt="log" width="30%">
-                        </div>
-                        <div class="container">
-                            <label><b>Email</b></label>
-                            <input type="email" placeholder="Enter Email" name="email" required>
-                            <label><b>Password</b></label>
-                            <input type="pass" placeholder="Enter Password"  name="password" required>
-                        </div>
-                        <div class="container registerc" style="background-color:#f1f1f1">
-                            <button class="loginb" type="submit">Login</button>
-                            <label>Not a member?</label>
-                            <button type="button" class="loginb regbtn" onclick="window.location.href='register.php';">Register Now!</button>
-                        </div>
-                    </form>
-                </div>
-        </nav>
+            </div>
+            <?php            
+            } else {
+            ?>
+            <button class="login-btn" type="button" onclick="document.getElementById('id01').style.display='block'">Login</button>
+            <?php } ?>
+            
+            <!-- Logo to the left -->
+      <a class="navbar-brand js-scroll-trigger" href="index.php"><img src ="img/Logo33.png"width="80" height="40"/></a>
+            <!-- Navigation Mennu -->
+          <button class="navbar-toggler navbar-toggler-left" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fa fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav text-uppercase ml-auto text-left">
+                <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#about">About us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#portfolio">Categories</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="faq.html">FAQ</a>
+              </li>
+          </ul>
+     
+        </div>
+      </div>
+        <!-- The Modal -->
+        <div id="id01" class="modal">
+            
+        <!-- Modal Content -->
+            
+        <form class="loginform modal-content animate" action="login.php" method="post">
+         <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+        <div class="imgcontainer">
+        <img src="img/Logo33.png" alt="log" width="30%" height="">
+        </div>
+        <div class="container">
+        <label><b >E-mail</b></label>
+        <input type="email" placeholder="Enter Email" name="email" required>
+        <label><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="password" required>
+        </div>
+        <div class="container registerc" style="background-color:#f1f1f1">
+        <button class="loginb" type="submit">Login</button>
+        <label>Not A member?</label>
+        <button type="button" class="loginb regbtn"  onclick= "window.location.href='register.php';" >Register Now!</button>
+        </div>
+        </form>
+        </div>
+   </nav>
         
         <section class="bg-light">
             <div class="container">
@@ -204,9 +208,9 @@ session_start();
                 </div>
             </div>
         </section>
-        
+
     <!-- Footer -->
-    <footer>
+    <footer  style="background-color:white">
       <div class="container">
         <div class="row">
           <div class="col-md-4">
@@ -234,5 +238,7 @@ session_start();
         </div>
       </div>
     </footer>
-    </body>
+   
+  </body>
+
 </html>
