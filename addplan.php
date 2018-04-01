1c0f84fde43c2bfc29c381dcffab4d7a3f52d394
muneerahalasqah <30496874+muneerahalasqah@users.noreply.github.com>
2018-04-01 22:24:39 +0300

    fixed

--- addplan.php
+++ addplan.php
@@ -1,12 +1,6 @@
 <?php
 session_start();
-
-if(empty($_SESSION['plan'])) {
-   $_SESSION['plan']=array(); 
-}
-
-if (in_array($_GET['id'],$_SESSION['plan'])){
-        ?>
+?>
 <html>
     <head>
     <script src="js/sweetalert2.js"></script>
@@ -17,17 +11,24 @@ if (in_array($_GET['id'],$_SESSION['plan'])){
       crossorigin="anonymous"></script>
     </head>
 <body>
+<?php
+if(empty($_SESSION['plan'])) {
+   $_SESSION['plan']=array(); 
+}
+
+if (in_array($_GET['id'],$_SESSION['plan'])){
+        ?>
     <script>
     swal({title:"This vendor already exists in the plan!",type: "warning"}).then(function(){window.location.href = "cards.php?cid=<?php echo $_GET['cid']?>";});
     </script>
-
 <?php
     
 } else {
     array_push($_SESSION['plan'],$_GET['id']);
     ?>
     <script>
-    swal({title:"Done!",text:"Successfully added to the Party Plan", type: "success"}).then(function(){window.location.href = "cards.php?cid=<?php echo $_GET['cid']?>";});
+        swal({title:"Done!",text:"Successfully added to the Party Plan",type: "success",showConfirmButton: false,
+  timer: 2000}).then(function(){window.location.href = "cards.php?cid=<?php echo $_GET['cid']?>";});
     </script>
 <?php
 }
