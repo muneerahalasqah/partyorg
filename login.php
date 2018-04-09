e6936638138ecf9abd4ad6f9ab732c08baedf787
muneerahalasqah <30496874+muneerahalasqah@users.noreply.github.com>
2018-04-09 21:21:41 +0300

    login changed

--- login.php
+++ login.php
@@ -3,25 +3,7 @@ session_start();
 
 require 'connect.php';
 
-
-// إذا نجح العضو في ملأ الإستمارة
-if (isset($_POST['email']) AND isset($_POST['password']))
-{
-   // ...
-   $email = $_POST['email'];
-   $password = $_POST['password'];
-   // ...
-   // أخذ البيانات من القاعدة إعتمادا على ايميل العضو 
-   $response= $db->prepare('SELECT *
-              FROM customer
-              WHERE email = :mail
-            ');
-    $response->bindValue(':mail',$email,PDO::PARAM_STR);
-    $response->execute();
-    $member = $response->Fetch();
-    $response->CloseCursor();
-    
-    ?>
+?>
 <html>
     <head>
     <script src="js/sweetalert2.js"></script>
@@ -33,41 +15,47 @@ if (isset($_POST['email']) AND isset($_POST['password']))
     </head>
     <body>
 <?php
-   // التأكد من أن العضو موجود في قاعدة البيانات 
-   if(!$member){
+
+
+// إذا نجح العضو في ملأ الإستمارة
+if (isset($_POST['email']) AND isset($_POST['password']))
+{
+   // ...
+   $email = $_POST['email'];
+   $password = $_POST['password'];
+    // أخذ البيانات من القاعدة إعتمادا على ايميل العضو 
+    $check="SELECT * FROM customer WHERE email='$email'";
+    $result=mysqli_query($db,$check);
+    if(mysqli_num_rows($result)>0){
+        $row=mysqli_fetch_assoc($result);
+        if($password==$row['password']){
+            $_SESSION['cid'] = $row['customer_id'];
+            $fname= $row['fname'];
+            ?>
+        <script>
+              swal({title:"Welcome, <?php echo $fname; ?>",text:"You logged in successfully, redirecting you to Besher",type: "success",showConfirmButton: false,
+  timer: 2000}).then(function(){window.location.href = "index.php";});
+        </script>
+        <?php
+        } else {
+            ?>
+        <script>
+            swal({title:"Incorrect!",text:"You entered the wrong password",type: "error"}).then(function(){window.location.href = "index.php";});
+        </script>
+        <?php 
+        }
+    } else {
         ?>
             <script>
             swal({title:"Sorry,",text:"No user reistered with this email",type: "error"}).then(function(){window.location.href = "index.php";});
             </script>
         <?php  
-   }
-
-    // مقارنة كلمة المرور التي أرسلها العضو بالموجودة في القاعدة 
-   else if($password != $member['password']){     
-        ?>
-        <script>
-            swal({title:"Incorrect!",text:"You entered the wrong password",type: "error"}).then(function(){window.location.href = "index.php";});
-        </script>
-        <?php 
-   } else {
-        //ذا كان كل شيء على ما يرام . نقوم بإنشاء متغير الجلسة وطبع رسالة ترحيبية 
-   $_SESSION['cid'] = $member['customer_id'];
-   $fname= $member['fname'];
-   // ثم تحويله تلقائيا إلى أي صفحة نريد 
-    ?>
-            <script>
-             swal({title:"Welcome, <?php echo $fname; ?>",text:"You logged in successfully, redirecting you to Besher",type: "success",showConfirmButton: false,
-  timer: 2000}).then(function(){window.location.href = "index.php";});
-            </script>
-  <?php  
+    }
        
    }
     ?>
     </body>
 </html>
-        <?php
-                                                 
-}
-?>
+
 
 	
