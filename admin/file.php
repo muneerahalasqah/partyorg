ffc90b14e444f9d243651e23d6516a918e997464
nora-os <35890816+nora-os@users.noreply.github.com>
2018-04-16 11:05:18 +0300

    Update file.php

--- admin/file.php
+++ admin/file.php
@@ -15,19 +15,16 @@ if (isset($_POST['upload'])){
         for($i=0;$i<$count;$i++){
             $sample=$_FILES['samples']['name'][$i];
             $path=$location.$sample;
-            echo $path;
-            print_r($_FILES['samples']['tmp_name']);
             
             //يرفع الفايلات
             $result=move_uploaded_file($_FILES['samples']['tmp_name'][$i],$path);
             if($result){
-                echo "inside the if!";
                 mysqli_query($db,"INSERT INTO samples(sample_path,v_id) VALUES('".$path."', '".$vid."')");
             }
         }?>
         <script>
             alert('The vendor samples have been uploaded successfully');
-            //window.location.href="vendor.php";
+            window.location.href="vendor.php";
     </script>
     <?php
 }
