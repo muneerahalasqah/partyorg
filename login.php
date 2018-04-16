3aadd1f85104ce4e1c1453538029ee93e067ce21
muneerahalasqah <30496874+muneerahalasqah@users.noreply.github.com>
2018-04-16 11:23:10 +0300

    sweetalert fix3

--- login.php
+++ login.php
@@ -20,8 +20,8 @@ require 'connect.php';
       integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
 
     <script src="js/sweetalert2.js"></script>
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert2.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.js"></script>
+    <script src="https://unpkg.com/sweetalert2@7.18.0/dist/sweetalert2.all.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/promise-polyfill@7.1.0/dist/promise.min.js"></script>
     </head>
     <body>
 <?php
