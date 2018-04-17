24cce4c9bd6dfcd3d12d9b054d821fe274af6690
muneerahalasqah <30496874+muneerahalasqah@users.noreply.github.com>
2018-04-17 18:17:37 +0300

    messages fix

--- js/contact_me.js
+++ js/contact_me.js
@@ -10,6 +10,7 @@ $(function() {
       // get values from FORM
       var name = $("input#name").val();
       var email = $("input#email").val();
+      var phone = $("input#phone").val();
       var message = $("textarea#message").val();
       var firstName = name; // For Success/Failure Message
       // Check for white space in name for Success/Fail message
@@ -19,10 +20,11 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "../mail/contact_me.php",
+        url: "././mail/contact_me.php",
         type: "POST",
         data: {
           name: name,
+          phone: phone,
           email: email,
           message: message
         },
