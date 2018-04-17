93815eb689ff63f954719676d530bd68fe67dda7
muneerahalasqah <30496874+muneerahalasqah@users.noreply.github.com>
2018-04-17 22:41:01 +0300

    sweetalert

--- logout.php
+++ logout.php
@@ -5,22 +5,7 @@ session_destroy();
 ?>
 <html>
      <head>
-    <link rel="stylesheet" href="css/sweetalert2.css">
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-        <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Plugin JavaScript -->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-    
-        <script
-      src="https://code.jquery.com/jquery-2.2.4.js"
-      integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
-
-    <script src="js/sweetalert2.js"></script>
-    <script src="https://unpkg.com/sweetalert2@7.18.0/dist/sweetalert2.all.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/promise-polyfill@7.1.0/dist/promise.min.js"></script>
+        <script src="https://unpkg.com/sweetalert2@7.18.0/dist/sweetalert2.all.js"></script>
     </head>
     <body>
         <script>
